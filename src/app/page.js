bdf5fd96276786bcc2db2311afcdcedd4936c0e9
eugenexu <eugenexu@umich.edu>
2025-01-10 03:28:12 -0500

    remove background?

--- src/app/page.js
+++ src/app/page.js
@@ -32,20 +32,10 @@ function App() {
       <main>
         {activeTab === 'quiz' ? <QuizGenerator /> : <FinancialChatbot />}
       </main>
+      /*
     <div className="ocean">
-    <div className="bubble bubble--1"></div>
-    <div className="bubble bubble--2"></div>
-    <div className="bubble bubble--3"></div>
-    <div className="bubble bubble--4"></div>
-    <div className="bubble bubble--5"></div>
-    <div className="bubble bubble--6"></div>
-    <div className="bubble bubble--7"></div>
-    <div className="bubble bubble--8"></div>
-    <div className="bubble bubble--9"></div>
-    <div className="bubble bubble--10"></div>
-    <div className="bubble bubble--11"></div>
-    <div className="bubble bubble--12"></div>
     <div id="octocat"></div>
+
 </div>
     </div>
   );
